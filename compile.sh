375370fb7e53a49dafce8bd274c793c5c5234830
Elias Antoun <eliasantoun07@outlook.com>
2026-07-05 14:59:43 +0300

    fix: added CleaningRobot to the compilation script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Always run from the directory this script lives in
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$DIR"
+
 g++ -std=c++17 -Wall -Wextra \
-    src/robot.cpp src/task.cpp src/mobile_robot.cpp src/fleet.cpp \
+    src/robot.cpp src/task.cpp src/mobile_robot.cpp \
+    src/cleaning_robot.cpp src/cooking_robot.cpp src/fleet.cpp \
     main.cpp \
     -I include \
     -pthread \
